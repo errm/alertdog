693e2ed91c2881672143bb6d84d787a8cf3d285d
Ed Robinson <edward-robinson@cookpad.com>
2026-09-09 15:57:19 +0100

    Zero out PagerDutyKey as soon as we have used it

--- pkg/alertdog/alertdog.go
+++ pkg/alertdog/alertdog.go
@@ -60,6 +60,7 @@ func (a *Alertdog) UnmarshalYAML(unmarshal func(interface{}) error) error {
 func (a *Alertdog) Setup() {
 	a.alertmanager = alertmanager.Alertmanager{Endpoints: a.AlertmanagerEndpoints, Expiry: a.CheckInterval * 2}
 	a.pagerduty = pagerduty.New(a.PagerDutyKey, a.PagerDutyRunbookURL, nil)
+	a.PagerDutyKey = ""
 }
 
 func (a *Alertdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
